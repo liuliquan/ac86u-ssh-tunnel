dbae1501cf1136003038d7ee59f5f06513cae371
Liquan Liu <leviastan@gmail.com>
2017-10-19 16:20:06 +0800

    route local dns

--- dns/dnsmasq.sh
+++ dns/dnsmasq.sh
@@ -5,6 +5,11 @@ do
   eval "${env}"
 done
 
+echo "server=127.0.0.1" > /jffs/opt/etc/resolv.conf
+ln -sf /jffs/opt/etc/resolv.conf /etc/resolv.conf
+
+echo "server=$ORIGINAL_DNS" > /tmp/resolv.conf
+
 echo "server=$ORIGINAL_DNS" > /tmp/resolv.dnsmasq
 
 for domain in `cat /jffs/ac86u-ssh-tunnel/dns/tunnel-domains.txt`;
